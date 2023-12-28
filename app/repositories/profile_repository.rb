df9037a38cff189744fd61e9278fdfb5a59443c0
Rafael Silva <rafaelrsilva.dev@gmail.com>
2023-12-28 12:16:15 -0300

    search method (profile repo)

--- app/repositories/profile_repository.rb
+++ app/repositories/profile_repository.rb
@@ -9,4 +9,12 @@ class ProfileRepo
   def add(profile)
     @profiles << profile
   end
+
+  def search(id, user, email)
+    @profiles.each do |profile|
+      return profile if profile.id.eql?(id) || profile.user.eql?(user) || profile.email.eql?(email)
+    end
+
+    nil
+  end
 end
