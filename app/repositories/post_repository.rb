79363640aea4bcffe6ea671554561c8670e7b338
Rafael Silva <rafaelrsilva.dev@gmail.com>
2023-12-28 12:39:44 -0300

    search method (post repo)

--- app/repositories/post_repository.rb
+++ app/repositories/post_repository.rb
@@ -12,4 +12,18 @@ class PostRepo
     @posts << post
     post.profile.posts << post
   end
+
+  def search(id, text, date, profile, hashtag)
+    @posts.each do |post|
+      if advanced_post?(post, hashtag)
+        return post
+      elsif post.id.eql?(id) || post.text.include?(text) || post.date.eql?(date) || post.profile.eql?(profile)
+        return post
+      end
+    end
+  end
+
+  def advanced_post?(post, hashtag)
+    post.hashtags.hashtag?(hashtag) if post.is_a?(AdvancedPost)
+  end
 end
