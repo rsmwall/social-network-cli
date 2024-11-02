3261796d9d396a6c392c50875b430ed9efbdbfbc
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-02 12:52:24 -0300

    feat: implement 'add' and 'search' in ProfileRepository

--- app/repositories/profile_repository.rb
+++ app/repositories/profile_repository.rb
@@ -1,8 +1,30 @@
 # frozen_string_literal: true
 
+require '../models/profile.rb'
+
 # class Profile Repository
 class ProfileRepository
   def initialize
-    @profiles = []  
+    @profiles = {}
+  end
+
+  def add(profile)
+    return if @profiles.key?(profile.id)
+
+    @profiles[profile.id] = profile
+  end
+
+  def search(params)
+    params = params.reject { |_, value| value.nil? }
+
+    profile = nil
+    @profiles.each do |key, value|
+      if params.all? { |param_key, param_value| value.send(param_key) == param_value }
+        profile = value
+        break
+      end
+    end
+
+    profile
   end
 end
