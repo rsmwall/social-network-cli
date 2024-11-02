089cbb6b014670c740ed7f98bd2c5d80fa6ce5a2
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-02 10:45:52 -0300

    feat: add Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,1 +1,15 @@
 # frozen_string_literal: true
+
+# class Post
+class Post
+  attr_reader :id, :text, :likes, :deslikes, :date, :profile
+
+  def initialize(params)
+    @id = params[:id]
+    @text = params[:text]
+    @likes = params[:likes]
+    @dislikes = params[:dislikes]
+    @date = params[:date]
+    @profile = params[:profile]
+  end
+end
