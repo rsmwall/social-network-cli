94b7429cc6ab00f6458f3fe6c27c4b6fecd7ce5f
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-02 10:30:53 -0300

    feat: add Profile class

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,13 +2,11 @@
 
 # class Profile
 class Profile
-  attr_accessor :posts
   attr_reader :id, :user, :email
 
-  def initialize(id, user, email, posts)
+  def initialize(id, user, email)
     @id = id
     @user = user
     @email = email
-    @posts = posts
   end
 end
