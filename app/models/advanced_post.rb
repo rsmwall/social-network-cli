16b32a2895b78db445793e5fdee600ef85801e54
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-02 10:46:24 -0300

    feat: add AdvancedPost class

--- app/models/advanced_post.rb
+++ app/models/advanced_post.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+require_relative 'post'
+
+# class Advanced Post
+class AdvancedPost < Post
+  attr_reader :hashtags, :remaining_views
+
+  def initialize(params, hashtags, remaining_views)
+    super(params)
+    @hashtags = hashtags
+    @remaining_views = remaining_views
+  end
+end
