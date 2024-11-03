857deaaee8961fba95c0e7cc4e7e6ebb6b4fa4ae
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-03 13:03:34 -0300

    feat: implement 'add' and 'search' in PostRepository

--- app/repositories/post_repository.rb
+++ app/repositories/post_repository.rb
@@ -9,4 +9,35 @@ class PostRepository
   def initialize
     @posts = {}
   end
+
+  def add(post)
+    return if @posts.key?(post.id)
+
+    @posts[post.id] = post
+  end
+
+  def search(params)
+    params = params.reject { |_, value| value.nil? }
+
+    post = nil
+    @posts.each do |key, value|
+      if params.all? do |param_key, param_value|
+          case param_key
+          when :text
+            value.text.include?(param_value)
+          when :hashtags
+            if value.instance_of?(AdvancedPost)
+              value.has_hashtag?(param_value)
+            end
+          else
+            value.send(param_key) == param_value
+          end
+        end
+        post = value
+        break
+      end
+    end
+
+    post
+  end
 end
