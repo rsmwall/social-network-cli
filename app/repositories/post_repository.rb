15cee70cd5018cb3a1880bf33879b39046956ff7
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-03 13:02:16 -0300

    feat: add PostRepository class

--- app/repositories/post_repository.rb
+++ app/repositories/post_repository.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+require_relative '../models/post'
+require_relative '../models/advanced_post'
+require_relative '../models/profile'
+
+# class Post Repository
+class PostRepository
+  def initialize
+    @posts = {}
+  end
+end
