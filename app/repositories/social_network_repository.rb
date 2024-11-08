65c2af0ba5c8823e168d777d105097bd19a06883
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-08 16:23:35 -0300

    feat: implement 'add_post' and 'search_post' in SocialNetwork

--- app/repositories/social_network_repository.rb
+++ app/repositories/social_network_repository.rb
@@ -21,4 +21,16 @@ class SocialNetwork
   def search_profile(params)
     @profile_repo.search(params)
   end
+
+  # post methods
+  def add_post(params)
+    return if params.any? { |_, value| value.nil? } ||
+      !@post_repo.search(id: params[:id]).nil?
+    
+    @post_repo.add(params)
+  end
+
+  def search_post(params)
+    @post_repo.search(params)
+  end
 end
