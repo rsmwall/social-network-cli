a5d7b41f928bc28babf7d7f3f51fb734c237f3d1
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-19 21:24:31 -0300

    reafctor: reorganize Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,13 +4,11 @@ require_relative 'post'
 
 # class Profile
 class Profile
-  attr_accessor :posts
-  attr_reader :id, :user, :email, :posts
+  attr_reader :id, :user, :email
 
   def initialize(params)
     @id = params[:id]
     @user = params[:user]
     @email = params[:email]
-    @posts = []
   end
 end
