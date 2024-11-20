2a40abfc9fc0de77619190df9f16f6e19ebb9baa
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-19 21:26:39 -0300

    refactor: reorganize App

--- app/app.rb
+++ app/app.rb
@@ -7,11 +7,9 @@ class App
   def initialize
     @social_network = SocialNetwork.new
     @menu = <<~MENU
-    1. Add Profile        2. Search Profile
-    3. Add Post           4. Search Post
-    5. Feed               6. Show Posts Profile
-    7. Show Posts Hashtag 8. Like 
-    9. Dislike            0. Exit
+    1. Add Profile  2. Search
+    3. Add Post          
+    0. Exit
     MENU
   end
 
@@ -30,12 +28,6 @@ class App
     when 1 then add_profile
     when 2 then search_profile
     when 3 then add_post
-    when 4 then search_post
-    when 5 then feed
-    when 6 then show_post_profile
-    when 7 then show_post_by_hashtag
-    when 8 then like
-    when 9 then dislike
     end
   end
 
@@ -45,44 +37,76 @@ class App
     menu if key == "\n"
   end
 
-  def build_search_params
-    accepted_fields = %i[id user email]
-    params = {}
-
-    accepted_fields.each do |field|
-      print "\nEnter #{field} (or press Enter to skip)\n> "
-      value = gets.chomp
-      value = value.to_i if field == 'id'
-      params[field] = value.empty? ? nil : value
-    end
-  end
+  # profile methods
 
   def add_profile
     puts "\n❖ Add Profile\n\n"
-    print "\nEnter an ID\n> "
-    id = gets.chomp.to_i
-    print "\nEnter an User\n> "
+    print "\nEnter User\n> "
     user = gets.chomp
-    print "\nEnter an E-mail\n> "
+    print "\nEnter E-mail\n> "
     email = gets.chomp
 
-    success = @social_network.add_profile(id: id, user: user, email: email)
+    success = @social_network.add_profile(user: user, email: email)
     puts success ? "\n\nProfile added successfully!" : "\n\nError adding profile!"
     enter_key
   end
 
   def search_profile
     puts "\n❖ Search Profile\n\n"
-    search_params = build_search_params
-    result = @social_network.search_profile(search_params)
+    print "\nEnter User\n> "
+    user = gets.chomp
+
+    result = @social_network.search_profile(user)
 
     if result
       puts "\nProfile found."
-      puts "\nID > #{result.id}\nUSER > @#{result.user}\nE-MAIL > #{result.email}"
+      result.each { |_, profile| puts "\nID > #{profile.id}\nUSER > @#{profile.user}\nE-MAIL > #{profile.email}" }
     else
       puts "\nNo profile found with the given criteria."
     end
 
+    enter_key
+  end
+
+  # posts methods
+
+  def add_post
+    puts "\n❖ Add Post\n\n"
+    print "\nEnter ID\n> "
+    id = gets.chomp.to_i
+    print "\nEnter Text\n> "
+    text = gets.chomp
+    print "\nEnter User\n> "
+    user = gets.chomp
+    
+    result = @social_network.search_profile(user: user)
+    if result.nil?
+      puts "\nNo profile found with the given criteria."
+      enter_key
+    end
+    success = nil
+
+    loop do
+      print "\nAdd hahstags (y/n): "
+      option = gets.chomp.downcase
+
+      if option == 'y'
+        print "\nEnter hashtags (separated by commas)\n> "
+        input = gets.chomp
+        hashtags = input.split(',').map(&:strip)
+
+        success = @social_network.add_post(
+          id: id, text: text, likes: 0, dislikes: 0, date: Time.now, profile: result,
+          hashtags: hashtags, remaining_views: 100)
+        break
+      elsif option == 'n'
+        success = @social_network.add_post(
+          id: id, text: text, likes: 0, dislikes: 0, date: Time.now, profile: result)
+        break
+      end
+    end
+
+    puts success ? "\n\nPost added successfully!" : "\n\nError creating post!"
     enter_key
   end
 end
