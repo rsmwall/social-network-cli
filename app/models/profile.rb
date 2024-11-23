4ce2c74eab495db438a3c2f0178e145c755fdb21
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-11-23 17:02:37 -0300

    feat: add 'add' method to posts and 'password' var in Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,11 +4,18 @@ require_relative 'post'
 
 # class Profile
 class Profile
-  attr_reader :id, :user, :email
+  attr_accessor :posts
+  attr_reader :id, :user, :email, :password
 
   def initialize(params)
     @id = params[:id]
     @user = params[:user]
     @email = params[:email]
+    @posts = []
+    @password = params[:email]
+  end
+
+  def add(post_id)
+    @posts << post_id
   end
 end
